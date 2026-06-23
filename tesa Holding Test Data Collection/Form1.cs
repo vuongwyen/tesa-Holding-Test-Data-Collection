808a40a6753756c8d8f12d29c7a1d073e1425af1
vuongwyen <treepoo2023@gmail.com>
2026-06-23 11:11:47 +0700

    Add project files.

---
+++ tesa Holding Test Data Collection/Form1.cs
@@ -0,0 +1,10 @@
+namespace tesa_Holding_Test_Data_Collection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
